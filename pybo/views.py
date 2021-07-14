0b057277ec6cf79c7d7bce5c04b02d1d522e553b
hadesyi <leesk55@gmail.com>
2021-07-14 19:48:08 +0900

    render로 화면 출력하기

--- pybo/views.py
+++ pybo/views.py
@@ -9,4 +9,4 @@ def index(request):
     """
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list': question_list}
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    return render(request, 'pybo/question_list.html', context)
