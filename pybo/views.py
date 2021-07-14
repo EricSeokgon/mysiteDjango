ca6c43598d31ae9fca8f998ce2e276a479511152
hadesyi <leesk55@gmail.com>
2021-07-14 20:45:04 +0900

    pybo/question_detail.html 작성하기

--- pybo/views.py
+++ pybo/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 from .models import Question
 
 
-def index(request):
+def detail(request, question_id):
     """
-    pybo 목록 출력
+    pybo 내용 출력
     """
-    question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
-    return render(request, 'pybo/question_list.html', context)
+    question = Question.objects.get(id=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
