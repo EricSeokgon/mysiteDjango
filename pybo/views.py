4fc375c4c856c63fd7d0212e79aa9d6fd3973e12
hadesyi <leesk55@gmail.com>
2021-07-14 19:47:40 +0900

    Question 모델 데이터 작성일시 역순으로 조회하기

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+
 
 def index(request):
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
     return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
