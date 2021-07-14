013907787186af0f9261a4553623b75f5e4233cf
hadesyi <leesk55@gmail.com>
2021-07-14 21:47:59 +0900

    AnswerForm 클래스 추가하고 answer_create 함수 수정하기

--- pybo/views.py
+++ pybo/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Question
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 
 
 def index(request):
@@ -29,8 +29,18 @@ def answer_create(request, question_id):
     pybo 답변등록
     """
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
 
 
 def question_create(request):
