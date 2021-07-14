9c5f2f9f4db3f9d37fcee0bd170989081d1e5f3d
hadesyi <leesk55@gmail.com>
2021-07-14 20:50:35 +0900

    answer_create 함수 추가하기

--- pybo/views.py
+++ pybo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 
 
 def detail(request, question_id):
@@ -11,3 +12,11 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
